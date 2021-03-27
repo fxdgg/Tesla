90cf7192d164afc608e26f863dc5d026aad176e4
fxdgg <783332991@qq.com>
2021-03-28 02:02:39 +0800

    Create Container.php

---
+++ Libs/Container.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tesla\Libs;
+
+use Closure;
+
+class Container
+{
+    private static $service;
+
+    public static function register($name, Closure $callback)
+    {
+        self::$service[$name] = $callback;
+    }
+
+    public static function book($name)
+    {
+        if (self::$service[$name] instanceof Closure) {
+            $callback = self::$service[$name];
+            return $callback();
+        }
+    }
+
+}
